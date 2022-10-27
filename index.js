9a5c785f79302bd0bbfb80c83e8e5cdb580a8607
Valeria Muhembele <61202849+anamivale@users.noreply.github.com>
2022-10-27 11:48:59 +0300

    task done

---
+++ index.js
@@ -0,0 +1,29 @@
+const fs = require("fs")
+const path = require("path")
+const http = require("http")
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/home") {
+    let filepath = path.join(__dirname, "Home.html")
+    fs.readFile(filepath, "utf8", (err, data) => {
+      res.end(data)
+    })
+  }
+  if (req.url === "/about") {
+    let filepath = path.join(__dirname, "About.html")
+    fs.readFile(filepath, "utf8", (err, data) => {
+      res.end(data)
+    })
+  }
+  if (req.url === "/contact") {
+    let filepath = path.join(__dirname, "Contact.html")
+    fs.readFile(filepath, "utf8", (err, data) => {
+      res.end(data)
+    })
+  }
+})
+const port = 4004
+
+server.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
